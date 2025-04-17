4695d2d6a824b26454a346e2c2942f47a09f9672
helljh <junho308917@naver.com>
2025-04-17 15:42:33 +0900

    Style: ColorStyles 정의

---
+++ image_search_app/lib/ui/color_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorStyles {
+  static const black = Colors.black;
+  static const white = Colors.white;
+
+  static const primaryColor = Color(0xff4a90e2);
+  static const secondaryColor = Color(0xff50e3c2);
+  static const accentColor = Color(0xfff5a623);
+  static const textColor = Color(0xff333333);
+}
